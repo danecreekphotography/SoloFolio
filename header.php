b15b236b2255de59f6724b8072eb3417a1519319
Joel Hawksley <joel@mojotech.com>
2014-10-03 13:49:40 -0400

    Version bump.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<!-- Powered by SoloFolio v6.20140927 - http://solofol.io -->
+<!-- Powered by SoloFolio v6.20141003 - http://solofol.io -->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0" />
