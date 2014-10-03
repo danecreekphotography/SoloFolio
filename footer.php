7c13365028f0267c275fb8fd40e077e4a85af432
Joel Hawksley <joel@mojotech.com>
2014-10-03 12:30:44 -0400

    [REVIEW CLEANUP] Move footer scripts to wp_enqueue_script.

--- footer.php
+++ footer.php
@@ -9,10 +9,6 @@ global $current_user;
 if ($current_user->user_level != 10 ) { echo get_theme_mod( 'solofolio_tracking' ); }
 ?>
 
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.retina.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.fitvids.min.js"></script>
-<script type="text/javascript"src="<?php echo get_template_directory_uri(); ?>/js/solofolio-base.js"></script>
-
 <?php wp_footer() ?>
 
 </body>
--- functions.php
+++ functions.php
@@ -73,6 +73,13 @@ function modify_jquery() {
 }
 add_action('init', 'modify_jquery');
 
+function solofolio_footer_scripts() {
+  wp_enqueue_script('jquery-retina', get_template_directory_uri().'/js/jquery.retina.min.js', array('jquery'), null, true);
+  wp_enqueue_script('jquery-fitvids', get_template_directory_uri().'/js/jquery.fitvids.min.js', array('jquery'), null, true);
+  wp_enqueue_script('solofolio-base', get_template_directory_uri().'/js/solofolio-base.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'solofolio_footer_scripts');
+
 // Disable Admin Bar from frontend - More trouble than it's worth
 function hide_admin_bar_from_front_end() {
   if (is_blog_admin()) {
