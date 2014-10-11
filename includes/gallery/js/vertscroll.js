3b2475927c482cf3f7a7e5febb46419fed553463
Joel Hawksley <joel@mojotech.com>
2014-10-10 21:55:16 -0400

    Fix bug where vertscroll gallery did not adapt to layout spacing.

--- includes/gallery/js/vertscroll.js
+++ includes/gallery/js/vertscroll.js
@@ -3,15 +3,16 @@ var setResponsive = function () {
   var headerHeight = jQuery("#header").outerHeight();
   var wrapperWidth = jQuery("#wrapper").innerWidth();
   var wrapperHeight = jQuery("#wrapper").outerHeight();
+  var pageSpacing = parseInt(jQuery('#wrapper').css('right'), 10);
 
   var n = jQuery("#header").css('right');
 
   if (wrapperWidth > 600) {
     if (n == '0px') {
-      jQuery('.vert-scroll img').css('max-height', pageHeight - headerHeight - 40);
+      jQuery('.vert-scroll img').css('max-height', pageHeight - headerHeight - pageSpacing - pageSpacing);
     }
     else {
-      jQuery('.vert-scroll img').css('max-height', pageHeight - 40);
+      jQuery('.vert-scroll img').css('max-height', pageHeight - pageSpacing - pageSpacing);
     }
   } else {
     jQuery('.vert-scroll img').css('max-height', pageHeight);
