ec98b5c88bfaf2bf7774dd722da338f37564b6df
Joel Hawksley <joel@mojotech.com>
2014-11-01 13:32:17 -0400

    7.0.4

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define("SOLOFOLIO_VERSION",     "7.0.3");
+define("SOLOFOLIO_VERSION",     "7.0.4");
 
 include_once("includes/gallery.php");         // Gallery shortcode replacement
 include_once("includes/social-widget.php");   // Social media widget
