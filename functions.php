d6937e573b43e96326273d95d2eef19d1b6ae1a2
Joel Hawksley <joel@mojotech.com>
2014-11-03 20:29:47 -0500

    [FIX] Fix bug where images were not properly centered in IE11.
    [Resolves: #106]

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define("SOLOFOLIO_VERSION",     "7.0.11");
+define("SOLOFOLIO_VERSION",     "7.0.12");
 
 include_once("includes/gallery.php");         // Gallery shortcode replacement
 include_once("includes/social-widget.php");   // Social media widget
