77e2a9a38f5ec69782968d4863a08608e73a5521
Joel Hawksley <joel@mojotech.com>
2014-11-07 16:24:38 -0500

    Highlight parent page when child page is open.
    [Resolves #116]

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define("SOLOFOLIO_VERSION",     "7.0.15");
+define("SOLOFOLIO_VERSION",     "7.0.16");
 
 include_once("includes/gallery.php");         // Gallery shortcode replacement
 include_once("includes/social-widget.php");   // Social media widget
