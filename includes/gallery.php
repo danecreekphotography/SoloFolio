985b970c1156b93c358b0cfa34b77926ae10c85f
Joel Hawksley <joel@mojotech.com>
2014-11-07 16:34:18 -0500

    Force vert-scroll gallery on about/parent pages.
    [Resolves #107]

--- includes/gallery.php
+++ includes/gallery.php
@@ -25,6 +25,8 @@ function solofolio_gallery_shortcode($output, $attr) {
 	if ( wp_is_mobile() ||
 			 is_home() ||
 			 is_single() ||
+			 is_page_template( 'about.php' ) ||
+			 is_page_template( 'parent.php' ) ||
 			 is_page_template( 'story.php' ) ||
 			 preg_match('/(?i)msie [1-8]/',$_SERVER['HTTP_USER_AGENT'])
 		 ) {
