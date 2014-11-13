3c50f1a33c6fe5790d3d01973f527ae2c409207e
Joel Hawksley <joel@mojotech.com>
2014-11-12 21:01:45 -0500

    Use layout spacing for vertscroll height restriction.
    [Resolves #137]

--- includes/gallery/js/vertscroll.js
+++ includes/gallery/js/vertscroll.js
@@ -3,16 +3,16 @@ var setResponsive = function () {
   var headerHeight = jQuery(".header").outerHeight();
   var wrapperWidth = jQuery(".wrapper").innerWidth();
   var wrapperHeight = jQuery(".wrapper").outerHeight();
-  var pageSpacing = parseInt(jQuery('.wrapper').css('right'), 10);
+  var layoutSpacing = solofolioVertScroll.layoutSpacing;
 
   var n = jQuery(".header").css('right');
 
   if (wrapperWidth > 600) {
     if (n == '0px') {
-      jQuery('.vert-scroll img').css('max-height', pageHeight - headerHeight - pageSpacing - pageSpacing);
+      jQuery('.vert-scroll img').css('max-height', pageHeight - headerHeight - layoutSpacing - layoutSpacing);
     }
     else {
-      jQuery('.vert-scroll img').css('max-height', pageHeight - pageSpacing - pageSpacing);
+      jQuery('.vert-scroll img').css('max-height', pageHeight - layoutSpacing - layoutSpacing);
     }
   } else {
     jQuery('.vert-scroll img').css('max-height', pageHeight);
